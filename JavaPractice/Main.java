7b1e006e151f9ab3917950565864462297226611
Lewis <lewistkbessong@gmail.com>
2026-03-11 20:39:49 -0500

    Added more strings and added print isntead of pintln

--- JavaPractice/Main.java
+++ JavaPractice/Main.java
@@ -1,10 +1,11 @@
-package JavaPractice;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" Lewis Bessong ");
+        System.out.println("Hello, Im lewis!");
+        System.out.println();
+        System.out.println("Im learning a new language for fun and for UIL!");
+        System.out.print("Hopefully I can do good");
 
     }
     
-
 }
