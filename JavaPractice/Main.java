aee2eca66a873651645f32d79668f5a8adb17120
Lewis <lewistkbessong@gmail.com>
2026-03-12 14:08:10 -0500

    Changed code to try and see whats wrong

--- JavaPractice/Main.java
+++ JavaPractice/Main.java
@@ -1,11 +1,5 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, Im lewis!");
-        System.out.println();
-        System.out.println("Im learning a new language for fun and for UIL!");
-        System.out.print("Hopefully I can do good");
-
+        System.out.println ("Hello!")
     }
-    
 }
